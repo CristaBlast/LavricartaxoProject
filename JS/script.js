4038cf354cb6505d964b3e017357d21f674ed7a8
Nuno Miguel <nm.albano20@gmail.com>
2021-03-23 14:40:49 +0000

    1.4

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,16 @@
-if(document.getElementById("menu"))
+function slideShow()
 {
-
+    var num = 1;
+    setInterval(() => 
+    {
+    if(num == 5)
+    {
+        num = 1;
+    }
+    else
+    {
+        num = num + 1;
+    }
+    document.getElementById("slideshowIMG").src = "Images/Slideshowtestimg" + num + ".jpg";
+    },3000);
 }
--- JS/weatherWidget.js
+++ JS/weatherWidget.js
@@ -3,7 +3,7 @@ if (conte)
 {
     conte.style.cssText = "width: 921px; color: #000000; background-color:transparent; border:1px solid transparent; margin: 0 auto; font-family: Arial;";
     elem = document.createElement("iframe");
-    elem.style.cssText = "width:910px; color:#000000; height:85px;";
+    elem.style.cssText = "width:910px; color:#000000; height:55px;";
     elem.id = "9787f05d8cb5e9af334b37cb1fb912d4";
     elem.src = "https://www.theweather.com/getwid/9787f05d8cb5e9af334b37cb1fb912d4";
     elem.frameBorder = 0;
