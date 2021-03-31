daf74e9273f6ffdf695e5a823eb2b9754077825b
Nuno Miguel <nm.albano20@gmail.com>
2021-03-31 13:01:49 +0100

    1.6

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,18 @@
-if(document.getElementById("menu"))
-{
 
+function slideShow()
+{
+    var num = 1;
+    setInterval(() => 
+    {
+        localStorage.clear();
+        if(num == 5)
+        {
+            num = 1;
+        }
+        else
+        {
+            num++;
+        }
+        document.getElementById("slideshowIMG").src = "Images/slideshow/" + num + ".jpg";    
+    },3000);    
 }
