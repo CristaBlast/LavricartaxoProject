b53c01fc8a150fb1fc60952a501222b9818b3924
Nuno Miguel <nm.albano20@gmail.com>
2021-04-20 10:25:51 +0100

    1.10

--- JS/script.js
+++ JS/script.js
@@ -16,16 +16,103 @@ function slideshow()
     setTimeout(slideshow, 2500);    
 }
 
-function sidebuttonR()
+/*s*/
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+/*/s*/
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+function openImageView()
 {
-    document.getElementsByClassName("nav").style.visibility = "visible";
-    document.getElementsByClassName("leftBtn").style.visibility = "visible";
-    document.getElementsByClassName("rightBtn").style.visibility = "hidden";
+    document.getElementById("myModal").style.display = "block";
 }
 
-function sidebuttonL()
+function closeImageView()
 {
-    document.getElementsByClassName("nav").style.visibility = "hidden";
-    document.getElementsByClassName("leftBtn").style.visibility = "hidden";
-    document.getElementsByClassName("rightBtn").style.visibility = "visible";
+    document.getElementById("myModal").style.display = "none";
 }
+
+function currentImage(n)
+{
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+
+// function sidebuttonR()
+// {
+//     document.getElementsByClassName("nav").style.visibility = "visible";
+//     document.getElementsByClassName("leftBtn").style.visibility = "visible";
+//     document.getElementsByClassName("rightBtn").style.visibility = "hidden";
+// }
+
+// function sidebuttonL()
+// {
+//     document.getElementsByClassName("nav").style.visibility = "hidden";
+//     document.getElementsByClassName("leftBtn").style.visibility = "hidden";
+//     document.getElementsByClassName("rightBtn").style.visibility = "visible";
+// }
